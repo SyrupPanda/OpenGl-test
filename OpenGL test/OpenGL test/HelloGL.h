7d21b185c019c131975db622f645ef6ce0c02128
Chris Dura <117527238+SyrupPanda@users.noreply.github.com>
2023-02-14 11:50:51 +0000

    Test enviroment

---
+++ OpenGL test/OpenGL test/HelloGL.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include "GL\freeglut.h"
+#include "GLUTCallBacks.h"
+
+class HelloGL
+{
+public:
+
+	//constructor definition
+	HelloGL(int argc, char* argv[]);
+
+	//destructor
+	~HelloGL(void);
+
+	void Display();
+};
+
